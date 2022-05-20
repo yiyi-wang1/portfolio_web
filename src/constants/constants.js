fb87919f0eb9d476b9e38bc97023f646109d83c9
yiyiwang <wang.yiyi1@northeastern.edu>
2022-05-20 00:37:41 -0700

    Adding Amazon project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,21 @@
 export const projects = [
   {
+    title: 'Amazon Clone',
+    description:"Amazon Clone App built with React, Expres, Firebase and Stripe API. You can register, login, browser product, checkout, pay(real payment!) and check your orders! Deloyed by Firebase hosting",
+    image: '/images/AmazonClone.png',
+    tags: ['React', 'Expres', 'Firebase', 'Stripe', 'Firestore'],
+    live: 'https://clone-89948.web.app/',
+    code: 'https://github.com/yiyi-wang1/amazon-clone',
+    id: 0,
+  },
+  {
     title: 'JoinUs Android App',
     description: "An event finding application for users to explore the public events nearby. Worked with APIs from Google Maps, Geocoding and other third parties. Utilized Google Firestore to store user app data, used Google Cloud Storage to store user content",
     image: '/images/JoinUsApp.png',
     tags: ['Java', 'Firebase','Android'],
     live: 'https://www.youtube.com/watch?v=HU0Ox9darAE',
     code: 'https://github.com/yiyi-wang1/JoinUs',
-    id: 0,
+    id: 1,
   },
   {
     title: 'Freecell Game',
@@ -15,7 +24,7 @@ export const projects = [
     tags: ['Java', 'Swing'],
     live: 'https://www.youtube.com/watch?v=q_RFfQHsSXc',
     code: 'https://github.com/yiyi-wang1/5004-freecell',
-    id: 1,
+    id: 2,
   },
 ];
 
